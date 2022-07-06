c56cc8c6ee708b176da6fae9eb62db93bf24ee37
beyzanurakin <akinbeyzanur@gmail.com>
2022-07-06 22:34:32 +0300

    Create context draft

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { createContext, useState, useEffect } from 'react'
+import { mockUser } from './mockData.js/mockUser'
+import { mockRepos } from './mockData.js/mockRepos'
+import { mockFollowers } from './mockData.js/mockFollowers'
+import axios from 'axios'
 
-function context() {
-  return <div>context</div>
+const rootUrl = 'https://api.github.com'
+
+const GithubContext = React.createContext()
+
+//Provider Consumer - GithubContext.Provider
+
+const GithubProvider = ({ children }) => {
+  return (
+    <GithubContext.Provider value={'hello'}>{children}</GithubContext.Provider>
+  )
 }
-
-export default context
+export { GithubProvider, GithubContext }
