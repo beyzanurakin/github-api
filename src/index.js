ce56596355bbc0bbc8c28184c3e470ba611a5364
beyzanurakin <akinbeyzanur@gmail.com>
2022-08-26 21:38:26 +0300

    DownGrade React version

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { GithubProvider } from './context/context'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <GithubProvider>
-    <App />
-  </GithubProvider>
+ReactDOM.render(
+  <React.StrictMode>
+    <GithubProvider>
+      <App />
+    </GithubProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 )
