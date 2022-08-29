4ca7394b36efeb58bad00ec29c232d87f9781530
beyzanurakin <akinbeyzanur@gmail.com>
2022-08-29 16:54:19 +0300

    Set Auth0 configuration

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { GithubProvider } from './context/context'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain='dev-4495qnxt.us.auth0.com'
+      clientId='ykJn7ut1X0TZZ3Os2esyCb4eKXaVrPPN'
+      redirectUri={window.location.origin}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
+    ,
   </React.StrictMode>,
   document.getElementById('root')
 )
