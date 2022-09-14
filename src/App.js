ee9278aa082b1041fdfb41173c88ffe5e8ffc480
beyzanurakin <akinbeyzanur@gmail.com>
2022-09-14 21:12:58 +0300

    Add the greatest wrapper to check authentication

--- src/App.js
+++ src/App.js
@@ -4,21 +4,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='login' element={<Login />} />
-        <Route
-          path='/'
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          }
-        />
-        <Route path='login' element={<Login />} />
-        <Route path='*' element={<Error />} />
-      </Routes>
-    </Router>
+    <AuthWrapper>
+      <Router>
+        <Routes>
+          <Route path='login' element={<Login />} />
+          <Route
+            path='/'
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route path='login' element={<Login />} />
+          <Route path='*' element={<Error />} />
+        </Routes>
+      </Router>
+    </AuthWrapper>
   )
 }
 
